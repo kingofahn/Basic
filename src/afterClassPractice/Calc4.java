6df2f79657d12890c6a55053d135b63d5b9024ad
kingofahn <kingofahn@gmail.com>
2018-06-04 20:28:04 +0900

    updated

---
+++ src/afterClassPractice/Calc4.java
@@ -0,0 +1,43 @@
+package afterClassPractice;
+import javax.swing.JOptionPane;
+public class Calc4 {
+	public static void main(String[] args) {
+		String num1 = JOptionPane.showInputDialog("num1?");
+		String result = num1;
+		int sum = Integer.parseInt(num1);
+		while(true) {
+			String num2 = "";
+			switch(JOptionPane.showInputDialog("OP")) {
+			case "+":
+				num2 = JOptionPane.showInputDialog("num2?");
+				result+= "+" + num2;
+				sum+= Integer.parseInt(num2);
+				break;
+			case "-":
+				num2 = JOptionPane.showInputDialog("num2?");
+				result+= "-" + num2;
+				sum-= Integer.parseInt(num2);
+				break;
+			case "/":
+				num2 = JOptionPane.showInputDialog("num2?");
+				result+= "/" + num2;
+				sum/= Integer.parseInt(num2);
+				break;
+			case "*":
+				num2 = JOptionPane.showInputDialog("num2?");
+				result+= "*" + num2;
+				sum*= Integer.parseInt(num2);
+				break;	
+			case "%":
+				num2 = JOptionPane.showInputDialog("num2?");
+				result+= "%" + num2;
+				sum%= Integer.parseInt(num2);
+				break;	
+			case "=":
+				JOptionPane.showMessageDialog(null,result + "=" + sum);
+				return;				
+			default : JOptionPane.showMessageDialog(null, "Error"); break;
+			}
+		}
+	}
+}
