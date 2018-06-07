5281ad519591651856a4783af790000faeb81ee6
kingofahn <kingofahn@gmail.com>
2018-06-07 09:30:14 +0900

    updated mabangjin

---
+++ src/step4/MagicSquare.java
@@ -0,0 +1,33 @@
+package step4;
+
+public class MagicSquare {
+	public static void main(String[] args) {
+	int S[][] = new int[5][5];
+	int R=0, C=2;
+	for(int N = 1;N<=25;N++){
+		S[R][C] = N;
+		int NR = R - 1;
+		int NC = C + 1;
+		if (NR == -1) {
+			NR = 4;
+		}
+		if (NC == 5) {
+			NC = 0;
+		}
+		if (S[NR][NC] == 0) {
+			R = NR;
+			C = NC;
+		} else {
+			R++;
+		}
+	}
+	
+		for (int NR = 0; NR <= 4; NR++) {
+			System.out.println();
+			for (int NC = 0; NC <= 4; NC++) {
+				System.out.print(S[NR][NC]+"\t");
+			}
+		}
+	}
+}
+
